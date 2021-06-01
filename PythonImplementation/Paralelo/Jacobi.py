fdef4cffac55801757b2d8f1d63a573619a0ebe7
rsaldarris <rsaldarris@eafit.edu.co>
2021-06-01 17:41:13 -0500

    Pythonisa Paralelo

--- PythonImplementation/Paralelo/Jacobi.py
+++ PythonImplementation/Paralelo/Jacobi.py
@@ -3,6 +3,7 @@ import numpy as np
 from ReadData import readMatrix, readVector
 import time
 from multiprocessing import Pool, TimeoutError, cpu_count, Process
+import threading
 
 '''
 Método que calcula (haciendo uso del paralelismo) la solución a un sistema de ecuaciones lineales por medio del método iterativo de Jacobi.
@@ -27,17 +28,22 @@ def jacobi(A, b, x, n, m, tolerance=1e-10, max_iterations=500):
     print(total)     
     return x
 '''
-def jacobiTask(A, b, x, n, m):
+def jacobiTask(A, b, c, n, m):
+    print("Entro",n," ",c)
     for i in range(n,m):
         suma = 0 
         for j in range(len(A[0])):
             if j != i:
                 
-                suma += A[i,j] * x[j]
+                suma += A[i,j] * c[j]
         
-        x[i] = (b[i] - suma) / A[i,i] 
-    
-    return x
+        c[i] = (b[i] - suma) / A[i,i] 
+    print("Salio",n," ",c)
+    # return c
+
+def cambio(b,z):
+    print("Entro")
+    b[1,1]=z
 
 if __name__ == '__main__':
     #Se crea la matriz A
@@ -49,24 +55,36 @@ if __name__ == '__main__':
     # initialize the RHS vector
     b = np.array([6., 25., -11., 15.])
     x = np.zeros_like(b, dtype=np.double)
+
+    # print(A)
+    # cambio(A,1)
+    # print(A)
+
     tolerance=1e-10
-    max_iterations=30
+    max_iterations=10
     new_x = x
-    with Pool(processes=2) as pool:
-        t0 = time.time()
-        
-        for k in range(max_iterations):
-            #x_old = new_x.copy() 
-            n = pool.apply_async(jacobiTask, (A,b,new_x,0,2,))
-            m = pool.apply_async(jacobiTask, (A,b,new_x,2,4,))      
-            n.wait()
-            m.wait()
-            print(n.get())
-            print(m.get())
-            new_x = np.add(n.get(),m.get())
-            #print(new_x)
-            #if np.linalg.norm(x - x_old, ord=np.inf) / np.linalg.norm(x, ord=np.inf) < tolerance: #se verifica la tolerancia para ver si hubo convergencia
-               #break
+    # with Pool(processes=2) as pool:
+    t0 = time.time()
+    
+    for k in range(max_iterations):
+        #x_old = new_x.copy()
+        print(new_x) 
+        # jacobiTask(A,b,new_x,0,2,)
+        # jacobiTask(A,b,new_x,2,4,)
+        # n = pool.apply_async(jacobiTask, (A,b,new_x,0,2,))
+        # m = pool.apply_async(jacobiTask, (A,b,new_x,2,4,))  
+        n = threading.Thread(target=jacobiTask, args=(A,b,new_x,0,2,))
+        n.start()    
+        m = threading.Thread(target=jacobiTask, args=(A,b,new_x,2,4,))
+        m.start()   
+        n.join()
+        m.join()
+        # print(n.get())
+        # print(m.get())
+        # new_x = np.add(n.get(),m.get())
+        #print(new_x)
+        #if np.linalg.norm(x - x_old, ord=np.inf) / np.linalg.norm(x, ord=np.inf) < tolerance: #se verifica la tolerancia para ver si hubo convergencia
+            #break
 
         t1 = time.time()     
         total = t1-t0
